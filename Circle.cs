76901666157e135fb521c0c46b90df8dface3c18
duydang2412 <duydang2412@gmail.com>
2021-11-15 23:30:30 +0700

    feat(Circle): implement Circle shape

---
+++ Circle.cs
@@ -0,0 +1,18 @@
+namespace graphical {
+    class Circle : Shape {
+        protected int radius;
+        public Circle(int x, int y, int radius, System.Drawing.Color color) : base(x, y, color) {
+            this.radius = radius;
+        }
+        override public void Draw(System.Drawing.Graphics graphic) {
+            // graphic.DrawEllipse(new System.Drawing.Pen(this.color, 2), this.x, this.y, this.radius, this.radius);
+            graphic.DrawEllipse(new System.Drawing.Pen(this.color, (float)(2 * Shape.ratio)), (int)(this.x * Shape.ratio), (int)(this.y * Shape.ratio), (int)(this.radius * Shape.ratio), (int)(this.radius * Shape.ratio));
+        }
+        public override bool IsPointCollided(int x, int y) {
+            double ratioRadius = (double)(this.radius * Shape.ratio);
+            double ratioX = (double)(this.x * Shape.ratio);
+            double ratioY = (double)(this.y * Shape.ratio);
+            return (System.Math.Sqrt(System.Math.Pow(ratioX + ratioRadius / 2 - x, 2) + System.Math.Pow(ratioY + ratioRadius / 2 - y, 2))) <= ratioRadius / 2;
+        }
+    }
+}
