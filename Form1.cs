cbccf137f4cc128d2c617b1c18780e230a383e7c
duydang2412 <duydang2412@gmail.com>
2021-11-16 13:16:42 +0700

    refactor(Form): delete graphics

--- Form1.cs
+++ Form1.cs
@@ -13,7 +13,7 @@ namespace graphical
 	public partial class Form1 : Form
 	{
 		private bool cursorState;
-		private Graphics graphics;
+		private Shape ghostShape;
 		private decimal ratio;
 		public Form1()
 		{
@@ -23,16 +23,13 @@ namespace graphical
 			this.KeyUp += new KeyEventHandler(Form1_KeyUp);
 			this.pictureBox.MouseClick += new MouseEventHandler(pictureBox_MouseClick);
 			this.pictureBox.MouseWheel += new MouseEventHandler(pictureBox_MouseWheel);
+			this.pictureBox.MouseMove += new MouseEventHandler(pictureBox_MouseMove);
 			this.pictureBox.Paint += new PaintEventHandler(pictureBox_Paint);
 		}
 		private void Form1_Load(object sender, EventArgs e) {
 			this.ratio = 1.0m;
 			this.cursorState = true;
 			this.Cursor = Cursors.Cross;
-			this.graphics = this.pictureBox.CreateGraphics();
-			this.graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-			this.graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
-			this.graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
 			this.SetStyle(ControlStyles.UserPaint, true);
 			this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
 			this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
@@ -66,7 +63,6 @@ namespace graphical
 			const int size = 70;
 			int offsetedX = (int)((e.Location.X - (int)(size * (double)(Shape.Ratio) / 2.0)) / Shape.Ratio);
 			int offsetedY = (int)((e.Location.Y - (int)(size * (double)(Shape.Ratio) / 2.0)) / Shape.Ratio);
-			// MessageBox.Show((e.Location.X - (int)(size / 2)) + " / " + Shape.Ratio + " => " + offsetedX);
 			new Circle(offsetedX, offsetedY, size, Color.Black);
 			this.pictureBox.Refresh();
 		}
@@ -79,10 +75,17 @@ namespace graphical
 				this.pictureBox.Refresh();
 			}
 		}
+		private void pictureBox_MouseMove(object sender, MouseEventArgs e) {
+			Shape shape = Shape.GetCollidedShape(e.Location.X, e.Location.Y);
+			if(shape != null) {
+
+			}
+		}
 		private void pictureBox_Paint(object sender, PaintEventArgs e) {
 			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
 			Shape.__Draw(e.Graphics);
+			ghostShape.Draw(e.Graphics);
 		}
 	}
 }
