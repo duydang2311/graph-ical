0e8fb5a445e158f1c5eef0703f87dc26a3daa6c0
duydang2412 <duydang2412@gmail.com>
2021-11-19 02:11:48 +0700

    feat(Hover): set cursor as hand if hovering

--- PictureBox/Hover.cs
+++ PictureBox/Hover.cs
@@ -5,16 +5,23 @@ namespace graphical {
 		private void pictureBox_MouseMove(object sender, MouseEventArgs e) {
 			Shape shape = Shape.GetCollidedShape(e.Location.X, e.Location.Y, this.ratio);
 			if(shape != null) {
+				if(this.cursorState == CursorStates.Add) {
+					this.Cursor = Cursors.Hand;
+				}
 				if(hoveringShape != null) {
 					hoveringShape.Hovering = false;
 				}
 				shape.Hovering = true;
 				hoveringShape = shape;
 				this.pictureBox.Refresh();
+				return;
 			} else if(hoveringShape != null) {
 				hoveringShape.Hovering = false;
 				hoveringShape = null;
 				this.pictureBox.Refresh();
+			}
+			if(this.cursorState == CursorStates.Add) {
+				this.Cursor = Cursors.Cross;
 			}
 		}
     }
