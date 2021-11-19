2aef2e43d2c0a6ae48a10f76d94a75c7ac362e08
duydang2412 <duydang2412@gmail.com>
2021-11-19 19:38:10 +0700

    feat(Util): overload Util.Normalize()

--- Util/Vector.cs
+++ Util/Vector.cs
@@ -7,5 +7,12 @@ namespace graphical {
             x /= dist;
             y /= dist;
         }
+        public static void Normalize(float aX, float aY, float bX, float bY, out float x, out float y) {
+            x = bX - aX;
+            y = bY - aY;
+            float dist = (float)System.Math.Sqrt(x * x + y * y);
+            x /= dist;
+            y /= dist;
+        }
     }
 }
