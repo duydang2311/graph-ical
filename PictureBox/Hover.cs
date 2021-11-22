3f13986cdb1853acc48ac662c4cdb29b99aacc38
duydang2412 <duydang2412@gmail.com>
2021-11-22 21:26:21 +0700

    refactor(Hover): create hoveringObject - support Line hover

--- PictureBox/Hover.cs
+++ PictureBox/Hover.cs
@@ -2,26 +2,51 @@ using System.Windows.Forms;
 
 namespace graphical {
 	public partial class Form1 : Form {
-		private void pictureBox_MouseMove(object sender, MouseEventArgs e) {
-			Shape shape = Shape.GetCollidedShape(e.Location.X, e.Location.Y, this.ratio);
-			if(shape != null) {
+		object hoveringObject;
+		private void Hover(object obj) {
+			if(obj == null) {
+				CancelHover();
+				return;
+			}
+			if(obj != null) {
 				if(this.cursorState == CursorStates.Add) {
 					this.Cursor = Cursors.Hand;
 				}
-				if(hoveringShape != null) {
-					hoveringShape.Hovering = false;
+				if(hoveringObject != null) {
+					if(hoveringObject is Shape) {
+						((Shape)hoveringObject).Hovering = false;
+					} else if(hoveringObject is Line) {
+						((Line)hoveringObject).Highlighted = false;
+					}
 				}
-				shape.Hovering = true;
-				hoveringShape = shape;
+				if(obj is Shape) {
+					((Shape)obj).Hovering = true;
+				} else if(obj is Line) {
+					((Line)obj).Highlighted = true;
+				}
+				hoveringObject = obj;
 				this.pictureBox.Refresh();
 				return;
-			} else if(hoveringShape != null) {
-				hoveringShape.Hovering = false;
-				hoveringShape = null;
-				this.pictureBox.Refresh();
 			}
+		}
+		private void CancelHover() {
+			if(hoveringObject is Shape) {
+				((Shape)hoveringObject).Hovering = false;
+			} else if(hoveringObject is Line) {
+				((Line)hoveringObject).Highlighted = false;
+			}
+			hoveringObject = null;
+			this.pictureBox.Refresh();
 			if(this.cursorState == CursorStates.Add) {
 				this.Cursor = Cursors.Cross;
+			}
+		}
+		private void pictureBox_MouseMove(object sender, MouseEventArgs e) {
+			Shape shape = Shape.GetCollidedShape(e.Location.X, e.Location.Y, this.ratio);
+			if(shape != null) {
+				Hover(shape);
+			} else {
+				Hover(Line.GetCollidedLine(e.Location.X, e.Location.Y, this.ratio));
 			}
 		}
     }
