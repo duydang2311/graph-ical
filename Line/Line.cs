a50cdcf5e4c707ea4cb3e2b96b863fa4ec5575e9
duydang2412 <duydang2412@gmail.com>
2021-11-23 00:30:17 +0700

    feat(Line): make ratio responsive

--- Line/Line.cs
+++ Line/Line.cs
@@ -73,39 +73,34 @@ namespace graphical {
             float tempX;
             float tempY;
             foreach(Line line in Line.lines) {
-                tempX = x - line.start.Anchor.X - line.start.OffsetX;
-                tempY = y - line.start.Anchor.Y - line.start.OffsetY;
+                tempX = x - line.start.Anchor.X * (float)ratio - line.start.OffsetX * (float)ratio;
+                tempY = y - line.start.Anchor.Y * (float)ratio - line.start.OffsetY * (float)ratio;
                 startDist = (float)System.Math.Sqrt(tempX * tempX + tempY * tempY);
-                tempX = x - line.end.Anchor.X - line.end.OffsetX;
-                tempY = y - line.end.Anchor.Y - line.end.OffsetY;
+                tempX = x - line.end.Anchor.X * (float)ratio - line.end.OffsetX * (float)ratio;
+                tempY = y - line.end.Anchor.Y * (float)ratio - line.end.OffsetY * (float)ratio;
                 endDist = (float)System.Math.Sqrt(tempX * tempX + tempY * tempY);
-                tempX = line.start.Anchor.X - line.end.Anchor.X;
-                tempY = line.start.Anchor.Y - line.end.Anchor.Y;
+                tempX = line.start.Anchor.X * (float)ratio - line.end.Anchor.X * (float)ratio;
+                tempY = line.start.Anchor.Y * (float)ratio - line.end.Anchor.Y * (float)ratio;
                 if((int)(startDist + endDist - (float)System.Math.Sqrt(tempX * tempX + tempY * tempY)) == 0) {
                     return line;
                 }
             }
             return null;
         }
-        public static int GetSideOfPoint(Line line, int x, int y) {
-           /*
-            * -1: start
-            *  0: middle
-            *  1: end
-            */
+        public static int GetSideOfPoint(Line line, int x, int y, decimal ratio) {
             float startDist;
             float percentage;
             float tempX;
             float tempY;
-            tempX = x - line.start.Anchor.X - line.start.OffsetX;
-            tempY = y - line.start.Anchor.Y - line.start.OffsetY;
-            startDist = (float)System.Math.Sqrt(tempX * tempX + tempY * tempY);
-            tempX = line.start.Anchor.X - line.end.Anchor.X;
-            tempY = line.start.Anchor.Y - line.end.Anchor.Y;
-            percentage = startDist / (float)System.Math.Sqrt(tempX * tempX + tempY * tempY);
-            if(percentage >= 0.8) {
+            tempX = x - line.start.Anchor.X * (float)ratio - line.start.OffsetX * (float)ratio;
+            tempY = y - line.start.Anchor.Y * (float)ratio - line.start.OffsetY * (float)ratio;
+            startDist = (float)System.Math.Sqrt(tempX * tempX + tempY * tempY) - line.start.OffsetX * (float)ratio;
+            tempX = line.start.Anchor.X * (float)ratio - line.end.Anchor.X * (float)ratio;
+            tempY = line.start.Anchor.Y * (float)ratio - line.end.Anchor.Y * (float)ratio;
+            percentage = startDist / ((float)System.Math.Sqrt(tempX * tempX + tempY * tempY) - line.start.OffsetX * (float)ratio - line.end.OffsetX * (float)ratio);
+            if(percentage >= 0.80) {
                 return 1;
-            } else if(percentage <= 0.2) {
+            } else if(percentage <= 0.20) {
                 return -1;
             }
             return 0;
