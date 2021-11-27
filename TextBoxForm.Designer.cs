1e487acbcf95cc061bfc2eba75180fe94dccacf8
duydang2412 <duydang2412@gmail.com>
2021-11-27 20:50:49 +0700

    feat(TextBoxForm): add TextBoxForm

---
+++ TextBoxForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace graphical
+{
+	partial class TextBoxForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxInput = new System.Windows.Forms.TextBox();
+			this.buttonOK = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			//textBoxInput 
+			// 
+			this.textBoxInput.Location = new System.Drawing.Point(13, 13);
+			this.textBoxInput.Name = "textBoxInput";
+			this.textBoxInput.Size = new System.Drawing.Size(156, 23);
+			this.textBoxInput.TabIndex = 0;
+			// 
+			//buttonOK 
+			// 
+			this.buttonOK.Location = new System.Drawing.Point(13, 42);
+			this.buttonOK.Name = "buttonOK";
+			this.buttonOK.Size = new System.Drawing.Size(75, 23);
+			this.buttonOK.TabIndex = 1;
+			this.buttonOK.Text = "OK";
+			this.buttonOK.UseVisualStyleBackColor = true;
+			// 
+			//buttonCancel 
+			// 
+			this.buttonCancel.Location = new System.Drawing.Point(94, 42);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.TabIndex = 2;
+			this.buttonCancel.Text = "Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			// 
+			// TextBoxForm
+			// 
+			this.AcceptButton = this.buttonOK;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.CancelButton = this.buttonCancel;
+			this.ClientSize = new System.Drawing.Size(182, 77);
+			this.ControlBox = false;
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonOK);
+			this.Controls.Add(this.textBoxInput);
+			this.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "TextBoxForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Vertex Name Prompt";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxInput;
+		private System.Windows.Forms.Button buttonOK;
+		private System.Windows.Forms.Button buttonCancel;
+	}
+}
---
+++ TextBoxForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphical
+{
+	public partial class TextBoxForm : Form
+	{
+		private DialogResult result;
+		public static TextBoxForm Prompt(Form owner, int startX, int startY, string inputText = "") {
+			TextBoxForm form = new TextBoxForm();
+			form.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			form.Location = new System.Drawing.Point(startX, startY);
+			form.InputText = inputText;
+			form.result = form.ShowDialog(owner);
+			return form;
+		}
+		public TextBoxForm() {
+			InitializeComponent();
+			this.FormClosing += new FormClosingEventHandler(TextBoxForm_Closing);
+			this.buttonOK.Click += new EventHandler(buttonOK_Click);
+		}
+		private void TextBoxForm_Closing(object sender, FormClosingEventArgs e) {
+		}
+		private void buttonOK_Click(object sender, EventArgs e) {
+			this.DialogResult = DialogResult.OK;
+		}
+		public DialogResult Result {
+			get => this.result;
+			set { this.result = value; }
+		}
+		public string InputText {
+			get => this.textBoxInput.Text;
+			set { this.textBoxInput.Text = value; }
+		}
+	}
+}
