14d0ff227006182bf0b1e8d30c93e78852ebd04d
duydang2412 <duydang2412@gmail.com>
2021-11-27 22:08:09 +0700

    chore(Util): disable ClampStringWidth due to low performance

--- Util/Size.cs
+++ Util/Size.cs
@@ -1,18 +1,19 @@
 namespace graphical {
     public partial class Util {
         public static string ClampStringWidth(string source, float width) {
-            string res = "";
-            float w = 0.0f;
-            System.Drawing.Font arial = new System.Drawing.Font("Arial", 20, System.Drawing.GraphicsUnit.Pixel);
-            for(int i = 0, size = source.Length; i != size; ++i) {
-                if(w >= width) {
-                    res += "\n";
-                    w = 0.0f;
-                }
-                res += source[i];
-                w += System.Windows.Forms.TextRenderer.MeasureText(source[i].ToString(), arial).Width;
-            }
-            return res;
+            // string res = "";
+            // float w = 0.0f;
+            // System.Drawing.Font arial = new System.Drawing.Font("Arial", 20, System.Drawing.GraphicsUnit.Pixel);
+            // for(int i = 0, size = source.Length; i != size; ++i) {
+            //     if(w >= width) {
+            //         res += "\n";
+            //         w = 0.0f;
+            //     }
+            //     res += source[i];
+            //     w += System.Windows.Forms.TextRenderer.MeasureText(source[i].ToString(), arial).Width;
+            // }
+            // return res;
+            return source;
         }
     }
 }
