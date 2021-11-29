340406383532a4146a3a65d438a0933a9a6ca81a
duydang2412 <duydang2412@gmail.com>
2021-11-29 13:59:48 +0700

    refactor(Paint): adjust InterpolationMode

--- PictureBox/Paint.cs
+++ PictureBox/Paint.cs
@@ -4,7 +4,7 @@ namespace graphical {
 	public partial class Form1 : Form {
 		private void pictureBox_Paint(object sender, PaintEventArgs e) {
 			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-			// e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
 			e.Graphics.Clear(System.Drawing.Color.White);
             Loop.__Draw(e.Graphics, this.ratio, this.pictureBox.Width, this.pictureBox.Height);
 			Shape.__Draw(e.Graphics, this.ratio, this.pictureBox.Width, this.pictureBox.Height);
