dbf72c3abb8481608e3ec977d9aacaf1822ffadc
duydang2412 <duydang2412@gmail.com>
2021-11-29 11:10:02 +0700

    feat(Form_About): add Form_About

---
+++ Form_About.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace graphical
+{
+	partial class Form_About
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_About));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.buttonClose = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label1.Location = new System.Drawing.Point(0, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(600, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "TRƯỜNG ĐẠI HỌC SƯ PHẠM KỸ THUẬT TP. HỒ CHÍ MINH";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label2.Location = new System.Drawing.Point(0, 50);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(600, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "KHOA CÔNG NGHỆ THÔNG TIN";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 80);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(600, 195);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.WaitOnLoad = true;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label3.Location = new System.Drawing.Point(0, 280);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(600, 23);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "TIỂU LUẬN CUỐI KỲ";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label4.Location = new System.Drawing.Point(0, 310);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(600, 23);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Môn học: Toán rời rạc";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label5.Location = new System.Drawing.Point(0, 340);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(600, 23);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Học kỳ 1, năm học 2021-2022";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label6.Location = new System.Drawing.Point(0, 385);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(600, 23);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "LẬP TRÌNH TẠO GIAO DIỆN ĐỒ HỌA NGƯỜI DÙNG,";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label7.Location = new System.Drawing.Point(0, 410);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(600, 23);
+			this.label7.TabIndex = 7;
+			this.label7.Text = "CHO PHÉP VẼ ĐỒ THỊ TRỰC QUAN";
+			this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label8.Location = new System.Drawing.Point(0, 455);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(600, 24);
+			this.label8.TabIndex = 8;
+			this.label8.Text = "Giảng viên hướng dẫn: PGS. TS. Hoàng Văn Dũng";
+			this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label9
+			// 
+			this.label9.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label9.Location = new System.Drawing.Point(0, 500);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(600, 24);
+			this.label9.TabIndex = 9;
+			this.label9.Text = "Sinh viên thực hiện";
+			this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label10.Location = new System.Drawing.Point(140, 530);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(92, 25);
+			this.label10.TabIndex = 10;
+			this.label10.Text = "20110438";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label11.Location = new System.Drawing.Point(140, 560);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(92, 25);
+			this.label11.TabIndex = 11;
+			this.label11.Text = "20110060";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label12.Location = new System.Drawing.Point(265, 530);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(208, 25);
+			this.label12.TabIndex = 12;
+			this.label12.Text = "Ngô Nguyên Quốc Anh";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label13.Location = new System.Drawing.Point(328, 560);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(145, 25);
+			this.label13.TabIndex = 13;
+			this.label13.Text = "Đặng Quốc Duy";
+			// 
+			// buttonClose
+			// 
+			this.buttonClose.Location = new System.Drawing.Point(528, 605);
+			this.buttonClose.Name = "buttonClose";
+			this.buttonClose.Size = new System.Drawing.Size(60, 23);
+			this.buttonClose.TabIndex = 14;
+			this.buttonClose.Text = "Close";
+			this.buttonClose.UseVisualStyleBackColor = true;
+			// 
+			// Form_About
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.buttonClose;
+			this.ClientSize = new System.Drawing.Size(600, 647);
+			this.Controls.Add(this.buttonClose);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form_About";
+			this.Text = "Form_About";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Button buttonClose;
+	}
+}
