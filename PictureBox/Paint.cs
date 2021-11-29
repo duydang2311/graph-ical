073ee60276f94d5159bbf573993cde3e34ba9699
duydang2412 <duydang2412@gmail.com>
2021-11-29 22:34:58 +0700

    feat(Paint): draw help elements

--- PictureBox/Paint.cs
+++ PictureBox/Paint.cs
@@ -4,11 +4,19 @@ namespace graphical {
 	public partial class Form1 : Form {
 		private void pictureBox_Paint(object sender, PaintEventArgs e) {
 			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+			e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+			e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+			e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
 			e.Graphics.Clear(System.Drawing.Color.White);
             Loop.__Draw(e.Graphics, this.ratio, this.pictureBox.Width, this.pictureBox.Height);
 			Shape.__Draw(e.Graphics, this.ratio, this.pictureBox.Width, this.pictureBox.Height);
             Line.__Draw(e.Graphics, this.ratio, this.pictureBox.Width, this.pictureBox.Height);
+			this.DrawHelpBackground(e.Graphics);
+			if(this.draggedObject == null) {
+				this.DrawHelpText(e.Graphics);
+			} else {
+				this.DrawHelpMouse(e.Graphics);
+			}
 		}
     }
 }
