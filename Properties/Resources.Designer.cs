0a19e549cb5b0dc74623d2301abb33d31a0c8549
duydang2412 <duydang2412@gmail.com>
2021-11-29 14:23:19 +0700

    feat(Properties): add hcmute logo to project resource

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace graphical.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hcmute_logo {
+            get {
+                object obj = ResourceManager.GetObject("hcmute-logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
